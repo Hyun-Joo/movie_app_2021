869dd7f49c034357a43bbb8865d4482074bb5bd9
Hyun-Joo <dlguswn714@gmail.com>
2021-01-31 16:25:07 +0900

    2nd

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+function Food({ name, picture }){
+  //console.log(props.fav);
+  return (
+    <div>
+      <h2>I like {name}.</h2>
+      <img src={picture} />
+    </div>
+  )
+}
+
+const foodILike = [
+  {
+    name: 'vongole',
+    image: 'https://www.the-pasta-project.com/wp-content/uploads/linguine-pasta-alle-vongole-linguine-with-clams-15-500x480.jpg'
+  },
+  {
+    name: 'aglio e olio',
+    image: 'https://www.simplydelisheats.com/uploads/2/4/2/3/24231242/img-7674.jpg'
+  },
+  {
+    name: 'pasta alla norma',
+    image: 'https://img.buzzfeed.com/thumbnailer-prod-us-east-1/video-api/assets/198417.jpg'
+  },
+  {
+    name: 'pomodoro',
+    image: 'https://www.onelovelylife.com/wp-content/uploads/2019/01/pomodoro-sauce14SM.jpg'
+  },
+  {
+    name: 'carbonara',
+    image: 'https://www.insidetherustickitchen.com/wp-content/uploads/2020/03/Spaghetti-alla-Carbonara-square-Inside-the-Rustic-Kitchen.jpg'
+  },
+  {
+    name: 'puttanesca',
+    image: 'https://data.thefeedfeed.com/static/other/15379753825baba456b674f.png'
+  }
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {foodILike.map(dish => 
+        <Food name={dish.name} picture={dish.image} />
+      )}
     </div>
   );
 }
